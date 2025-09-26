4ff9cb8b0738243ce9006ee70ca68566805a2c3e
Prathamesh Sanjay Kadam <135490330+Prathamesh-kadam@users.noreply.github.com>
2025-09-26 13:16:56 +0530

    Add files via upload

---
+++ Python Scripts/Unet_model.py
@@ -0,0 +1,199 @@
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Dropout
+from tensorflow.keras import Model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
+from IPython.display import clear_output
+
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+IMG_CHANNELS = 1
+def get_unet(input_shape):
+    inputs = tf.keras.layers.Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
+
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = tf.keras.layers.Dropout(0.1)(c1)
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = tf.keras.layers.Dropout(0.1)(c2)
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = tf.keras.layers.Dropout(0.2)(c3)
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = tf.keras.layers.Dropout(0.2)(c4)
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = tf.keras.layers.Dropout(0.3)(c5)
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+    u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = tf.keras.layers.Conv2D(4, (1, 1), activation='softmax')(c9)
+
+
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+  
+IMG_SIZE = (256, 256,1)
+cmap = plt.cm.get_cmap('viridis')   
+n_colors = len(cmap.colors)
+model = get_unet(IMG_SIZE) 
+tf.keras.utils.plot_model(model, show_shapes=True)
+
+def create_mask(pred_mask, ele=0):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[ele]
+
+def show_predictions(dataset=None, num=1, ele=0):
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = model.predict(image)
+            display([image[ele], mask[ele], create_mask(pred_mask, ele)])
+    else:
+        display([image, mask, create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+
+def plot_loss_acc(loss, val_loss, epoch):
+    
+    epochs = range(epoch+1)
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,5))
+
+    ax.plot(epochs, loss, 'r', label='Training loss')
+    ax.plot(epochs, val_loss, 'bo', label='Validation loss')
+    ax.set_title('Training and Validation Loss')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss Value')
+    ax.legend()
+    plt.show()
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        self.loss = []
+        self.val_loss = []
+    
+    def on_epoch_end(self, epoch, logs=None):
+        clear_output(wait=True)
+        
+        self.loss.append(logs['loss'])
+        self.val_loss.append(logs['val_loss'])
+        
+        show_predictions()
+        plot_loss_acc(self.loss, self.val_loss, epoch)
+        
+lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=8, verbose=1,)
+
+class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
+    def __init__(self,
+               y_true=None,
+               y_pred=None,
+               num_classes=None,
+               name=None,
+               dtype=None):
+        super(UpdatedMeanIoU, self).__init__(num_classes = num_classes,name=name, dtype=dtype)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.math.argmax(y_pred, axis=-1)
+        return super().update_state(y_true, y_pred, sample_weight)
+
+checkpoint_path = "training/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path, 
+    verbose=1, 
+    save_weights_only=True,
+    save_freq=5*BATCH_SIZE)
+
+my_callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=2),
+    tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
+    tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+]
+
+
+z_train_one_hot = to_categorical(z_train, num_classes=4)
+z_test_one_hot = to_categorical(z_test, num_classes=4)
+print(z_train_one_hot.shape)
+print(z_test_one_hot.shape)
+
+class CustomIoU(tf.keras.metrics.Metric):
+    def __init__(self, num_classes, name='custom_iou', **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.num_classes = num_classes
+        self.total_iou = self.add_weight(name='total_iou', initializer='zeros')
+        self.total_samples = self.add_weight(name='total_samples', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.argmax(y_pred, axis=-1)
+        y_true = tf.argmax(y_true, axis=-1)
+        iou = []
+        for i in range(self.num_classes):
+            intersection = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(y_true, i), tf.equal(y_pred, i)), dtype=tf.float32))
+            union = tf.reduce_sum(tf.cast(tf.logical_or(tf.equal(y_true, i), tf.equal(y_pred, i)), dtype=tf.float32))
+            iou.append(intersection / union)
+        self.total_iou.assign_add(tf.reduce_mean(iou))
+        self.total_samples.assign_add(1)
+
+    def result(self):
+        return self.total_iou / self.total_samples
+
+model=get_unet(IMG_SIZE)
+model.compile(optimizer=tf.keras.optimizers.Adam(), 
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False), 
+              metrics=['categorical_accuracy', CustomIoU(num_classes=4)],
+              run_eagerly=True)
+
+EPOCHS = 25
+VAL_SUBSPLITS = 5
+VALIDATION_STEPS = VAL_LENGTH//BATCH_SIZE//VAL_SUBSPLITS
+
+model_history = model.fit(x_train, 
+                          z_train_one_hot,
+                          epochs=EPOCHS,
+                          steps_per_epoch=STEPS_PER_EPOCH,
+                          validation_steps=VALIDATION_STEPS,
+                          validation_data=(x_test, z_test_one_hot),
+                          #callbacks=[DisplayCallback(), lr_callback, cp_callback]
+                         )
+
+val_loss = model_history.history['loss']
+val_acc = model_history.history['categorical_accuracy']
+
+print("Validation Loss:", val_loss)
+print("Validation Accuracy:", val_acc)
+
+
+
